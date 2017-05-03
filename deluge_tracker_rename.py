0a7a918ae5fe45ff61c7144f47ebb3683fd64f48
Wingjam <jo_martineau_7@hotmail.com>
2017-05-03 19:09:46 -0400

    Support regular expression url to rename trackers.
    Printed every unique state updated and NOT updated.
    Added Python .gitignore.

--- deluge_tracker_rename.py
+++ deluge_tracker_rename.py
@@ -4,10 +4,23 @@ import platform
 import shutil
 import pickle
 import argparse
+import re
+from sets import Set
+import time
 
 parser = argparse.ArgumentParser(description="Find and replace tracker urls in a Deluge torrents.state")
-parser.add_argument("orig_tracker_url", help="The original tracker url to be replaced.", type=str)
-parser.add_argument("new_tracker_url", help="The new tracker url to replace the original one.", type=str)
+parser.add_argument(
+    "orig_tracker",
+    help="The original tracker regular expression url (with n possible groups) to be replaced."
+         " i.e. https://tracker.t411.ai/(\w+)/announce",
+    type=str
+)
+parser.add_argument(
+    "new_tracker",
+    help="The new tracker url to replace the original one with \\n where n is the group number from orig_tracker."
+         " i.e. http://tracker.t411.al/\\1/announce",
+    type=str
+)
 args = parser.parse_args()
 
 if platform.system() in ('Windows', 'Microsoft'):
@@ -22,24 +35,38 @@ else:
     state_file_path = os.path.expanduser('~/.config/deluge/state/torrents.state')
 
 print("State file : {}".format(state_file_path))
-print("Replace '{}' with '{}'" .format(args.orig_tracker_url, args.new_tracker_url))
+print("Replace '{}' with '{}'".format(args.orig_tracker, args.new_tracker))
 state_file = open(state_file_path, 'rb')
 state = pickle.load(state_file)
 state_file.close()
 
-state_modified_count = 0
+state_updated = Set()
+state_not_updated = Set()
+
 for torrent in state.torrents:
     for tracker in torrent.trackers:
-        if tracker['url'] == args.orig_tracker_url:
-            tracker['url'] = args.new_tracker_url
-            state_modified_count += 1
+        new_string, number_of_subs_made = re.subn(args.orig_tracker, args.new_tracker, tracker['url'])
+        if number_of_subs_made:
+            tracker['url'] = new_string
+            state_updated.add(new_string)
+        else:
+            state_not_updated.add(new_string)
 
-
-if state_modified_count:
-    shutil.copyfile(state_file_path, state_file_path + '.old')
+if state_updated:
+    shutil.copyfile(state_file_path, "{}.{}.old".format(state_file_path, time.time()))
     state_file = open(state_file_path, 'wb')
     pickle.dump(state, state_file)
     state_file.close()
-    print("State Updated ({}/{})".format(state_modified_count, len(state.torrents)))
+
+    print("\nState Updated : {}".format(len(state_updated)))
+    for state in state_updated:
+        print(state)
+
+    print("\nState NOT Updated : {}".format(len(state_not_updated)))
+    for state in state_not_updated:
+        print(state)
 else:
-    print("Nothing to do")
+    print("Nothing to do ...")
+    print("\nState NOT Updated : {}".format(len(state_not_updated)))
+    for state in state_not_updated:
+        print(state)
